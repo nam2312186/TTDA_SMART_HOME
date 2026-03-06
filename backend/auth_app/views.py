e645b466f2de1afc2ffab3b09082662d4b4c3582
van080105 <van.nguyennhe2005@hcmut.edu.vn>
2026-03-06 20:16:40 +0700

    update auth & user app

--- backend/auth_app/views.py
+++ backend/auth_app/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from users_app.models import User
+from .serializers import RegisterSerializer, LoginSerializer
+from django.contrib.auth.hashers import check_password
+
+
+class RegisterView(APIView):
+
+    def post(self, request):
+
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save()
+
+            return Response({"message": "User created"})
+
+        return Response(serializer.errors)
+
+
+class LoginView(APIView):
+
+    def post(self, request):
+
+        serializer = LoginSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        username = serializer.validated_data["username"]
+
+        password = serializer.validated_data["password"]
+
+        try:
+
+            user = User.objects.get(username=username)
+
+            if check_password(password, user.password):
+
+                return Response({"message": "Login success"})
+
+        except User.DoesNotExist:
+
+            pass
+
+        return Response({"error": "Invalid credentials"})
